c9481981f2be14f84a1c145b11db8f8380467f17
Nosima <breno.nosima@gmail.com>
2026-05-14 19:21:34 -0300

    ex InlineTemp

---
+++ src/Atividade12/InlineTemp/Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2 {
+    public void verificarMedia(double prova1, double prova2) {
+        if (((prova1 + prova2) / 2) >= 6.0) {
+            System.out.println("Aluno aprovado");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+    }
+}
---
+++ src/Atividade12/InlineTemp/Exercicio4.java
@@ -0,0 +1,9 @@
+public class Exercicio4 {
+    public void verificarBonus(double salario) {
+        if (salario < 2500) {
+            System.out.println("Funcionário recebe bônus");
+        } else {
+            System.out.println("Funcionário não recebe bônus");
+        }
+    }
+}
